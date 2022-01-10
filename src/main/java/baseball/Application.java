2dca9c1141d34a07910680a33957899e6d36c140
SeungyeonJung <crowsy01@gmail.com>
2022-01-10 10:19:29 +0900

    랜덤 숫자 생성 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import nextstep.utils.Randoms;
+import nextstep.utils.Console;
+
 public class Application {
+
+    public String[] random() {
+        String[] arr = new String[3];
+        for(int i = 0; i < 3; i++){
+            arr[i] = String.valueOf(Randoms.pickNumberInRange(1, 9));
+        }
+
+        return arr;
+    }
+
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        Application application = new Application();
+        String[] randomNumberArr = application.random();
     }
 }
