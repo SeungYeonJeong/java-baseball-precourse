76cc403077c218fbd106e46f2d17cc4eda45e861
SeungYeon <crowsy01@gmail.com>
2022-01-10 13:15:44 +0900

    사용자 입력 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,8 @@ import nextstep.utils.Console;
 
 public class Application {
 
-    public String[] random() {
+    // 컴퓨터 난수 생성
+    public String[] getRandomNumber() {
         String[] arr = new String[3];
         for(int i = 0; i < 3; i++){
             arr[i] = String.valueOf(Randoms.pickNumberInRange(1, 9));
@@ -14,9 +15,19 @@ public class Application {
         return arr;
     }
 
+    // 사용자 입력
+    public String[] getInputNumber() {
+        System.out.print("숫자를 입력해 주세요 : ");
+        String inputNumber = Console.readLine();
+        String[] arr = inputNumber.split("");
+
+        return arr;
+    }
+
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
         Application application = new Application();
-        String[] randomNumberArr = application.random();
+        String[] randomNumberArr = application.getRandomNumber();
     }
+
 }
