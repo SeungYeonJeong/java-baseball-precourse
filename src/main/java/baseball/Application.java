804a2b73c702983bdbc5ad3af48c7c931e765027
SeungYeon <crowsy01@gmail.com>
2022-01-10 16:23:29 +0900

    스트라이크, 볼 개수 계산/반환 함수 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,13 +5,12 @@ import nextstep.utils.Console;
 
 public class Application {
 
-    // 컴퓨터 난수 생성
+    // 랜덤 숫자 생성
     public String[] getRandomNumber() {
         String[] arr = new String[3];
         while (true){
             for(int i = 0; i < 3; i++){
                 arr[i] = String.valueOf(Randoms.pickNumberInRange(1, 9));
-                System.out.println(arr[i]);
             }
             if (!arr[0].equals(arr[1]) && !arr[1].equals(arr[2]) && !arr[0].equals(arr[2])){
                 break;
@@ -30,27 +29,32 @@ public class Application {
         return arr;
     }
 
-    // 랜덤값, 입력값 비교 후 볼, 스트라이크 개수 반환
-    public int[] getCounts(String[] inputValue, String[] randomValue) {
+    // 스트라이크, 볼 개수 반환
+    public int[] compare(String[] inputValue, String[] randomValue) {
         int[] counts = new int[2];
 
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
-                if (i == j && inputValue[i].equals(randomValue[j])){
-                    counts[0]++;
-                    break;
-                }
-                if (i != j && inputValue[i].equals(randomValue[j])){
-                    counts[1]++;
-                    break;
-                }
+                counts = getCounts(inputValue, randomValue, i, j, counts);
             }
         }
 
         return counts;
     }
 
-    // 비교 결과 출력
+    // 스트라이크, 볼 개수 계산
+    public int[] getCounts(String[] inputValue, String[] randomValue, int i, int j, int[] counts){
+        if (i == j && inputValue[i].equals(randomValue[j])){
+            counts[0]++;
+        }
+        else if (i != j && inputValue[i].equals(randomValue[j])){
+            counts[1]++;
+        }
+
+        return counts;
+    }
+
+    // 스트라이크, 볼 개수 출력
     public boolean printCounts(int[] counts) {
         int countStrike = counts[0];
         int countBall = counts[1];
@@ -97,7 +101,7 @@ public class Application {
 
         while (true) {
             String[] inputNumberArr = application.getInputNumber();
-            int[] counts = application.getCounts(inputNumberArr, randomNumberArr);
+            int[] counts = application.compare(inputNumberArr, randomNumberArr);
             boolean isCorrect = application.printCounts(counts);
             if (!isCorrect){
                 continue;
